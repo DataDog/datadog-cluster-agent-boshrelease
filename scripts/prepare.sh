abc39106b3e772bd8c3079f7ff1d8b4ec05f8989
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-05-20 18:10:42 +0200

    Create dir before downloading

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,5 +8,6 @@ CLUSTER_AGENT_VERSION=${1:-$DEFAULT_CLUSTER_AGENT_VERSION}
 DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/linux/cluster-agent-cloudfoundry/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
 
 echo "Downloading ${DOWNLOAD_URL} ..."
+mkdir -p "$(dirname ${BLOBS_D}/${BLOB})"
 curl -L --fail "${DOWNLOAD_URL}" -o "${BLOBS_D}/${BLOB}"
 bosh add-blob "${BLOBS_D}/${BLOB}" "$BLOB"
