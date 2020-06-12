c0b5aac6f37409fd97a3ac781e7ade2cb979286f
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-06-12 12:03:18 +0200

    Release cluster agent 7.20.1

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,9 +3,9 @@ set -e
 
 BLOBS_D="$(pwd)/blobs"
 BLOB="datadog-cluster-agent/datadog-cluster-agent-cloudfoundry.tar.xz"
-DEFAULT_CLUSTER_AGENT_VERSION="7.20.0-rc.2.git.2.68e6468"
+DEFAULT_CLUSTER_AGENT_VERSION="7.20.1"
 CLUSTER_AGENT_VERSION=${1:-$DEFAULT_CLUSTER_AGENT_VERSION}
-DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/linux/cluster-agent-cloudfoundry/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
+DOWNLOAD_URL="http://s3.amazonaws.com/dd-agent/agent7/cluster-agent-cloudfoundry/linux/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
 
 echo "Downloading ${DOWNLOAD_URL} ..."
 mkdir -p "$(dirname ${BLOBS_D}/${BLOB})"
