c9abdf07d49013e1dc6bac680457dc936176a8fc
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2020-09-30 13:05:52 +0200

    Make prepare script able to download rc/devel versions (#12)
    * Make prepare script able to download rc/devel versions
    
    * Remove unnecessary env var

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,7 +5,11 @@ BLOBS_D="$(pwd)/blobs"
 BLOB="datadog-cluster-agent/datadog-cluster-agent-cloudfoundry.tar.xz"
 DEFAULT_CLUSTER_AGENT_VERSION="7.22.0"
 CLUSTER_AGENT_VERSION=${1:-$DEFAULT_CLUSTER_AGENT_VERSION}
-DOWNLOAD_URL="http://s3.amazonaws.com/dd-agent/agent7/cluster-agent-cloudfoundry/linux/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
+if echo "$CLUSTER_AGENT_VERSION" | grep -E "devel\.git\.|rc\."; then
+    DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/linux/cluster-agent-cloudfoundry/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
+else
+    DOWNLOAD_URL="http://s3.amazonaws.com/dd-agent/agent7/cluster-agent-cloudfoundry/linux/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
+fi
 
 echo "Downloading ${DOWNLOAD_URL} ..."
 mkdir -p "$(dirname ${BLOBS_D}/${BLOB})"
