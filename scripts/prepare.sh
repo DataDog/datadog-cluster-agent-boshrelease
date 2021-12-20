9049c5f3f7c7790674afd10b33c6d89554c9f9bf
NouemanKHAL <noueman.khal@gmail.com>
2021-12-20 13:18:21 +0100

    Release 1.9.2

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,7 +3,7 @@ set -e
 
 BLOBS_D="$(pwd)/blobs"
 BLOB="datadog-cluster-agent/datadog-cluster-agent-cloudfoundry.tar.xz"
-DEFAULT_CLUSTER_AGENT_VERSION="7.32.2"
+DEFAULT_CLUSTER_AGENT_VERSION="7.32.3"
 CLUSTER_AGENT_VERSION=${1:-$DEFAULT_CLUSTER_AGENT_VERSION}
 if echo "$CLUSTER_AGENT_VERSION" | grep -E "devel\.git\.|rc\."; then
     DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/linux/cluster-agent-cloudfoundry/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
