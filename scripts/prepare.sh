bcc363951d2b1e40c486e0025b8c5a6a857941a1
NouemanKHAL <noueman.khal@gmail.com>
2022-04-13 16:14:50 +0200

    bump agent version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,7 +3,7 @@ set -e
 
 BLOBS_D="$(pwd)/blobs"
 BLOB="datadog-cluster-agent/datadog-cluster-agent-cloudfoundry.tar.xz"
-DEFAULT_CLUSTER_AGENT_VERSION="7.33.0"
+DEFAULT_CLUSTER_AGENT_VERSION="7.35.1"
 CLUSTER_AGENT_VERSION=${1:-$DEFAULT_CLUSTER_AGENT_VERSION}
 DOWNLOAD_URL="http://s3.amazonaws.com/dsd6-staging/linux/cluster-agent-cloudfoundry/datadog-cluster-agent-cloudfoundry-${CLUSTER_AGENT_VERSION}-amd64.tar.xz"
 echo "Downloading ${DOWNLOAD_URL} ..."
